b8f4fb14c65f906fb69846a62973407f44a50e89
zhangzhuo <zhangzhuo@jyd.com.cn>
2020-12-25 09:15:20 +0800

    提交遗漏代码

---
+++ springboot-plugin-framework/src/main/java/com/gitee/starblues/integration/pf4j/ConfigPluginStatusProvider.java
@@ -0,0 +1,54 @@
+package com.gitee.starblues.integration.pf4j;
+
+import org.pf4j.PluginStatusProvider;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zhangzhuo
+ * @version 1.0
+ * @since 2020-12-24
+ */
+public class ConfigPluginStatusProvider implements PluginStatusProvider {
+
+    private Set<String> disabledPlugins = new HashSet<>();
+
+    public ConfigPluginStatusProvider() {
+        this(null);
+    }
+
+    public ConfigPluginStatusProvider(Set<String> disabledPluginIds) {
+        if(disabledPluginIds != null && !disabledPluginIds.isEmpty()){
+            this.disabledPlugins.addAll(disabledPluginIds);
+        }
+    }
+
+
+    @Override
+    public boolean isPluginDisabled(String pluginId) {
+        if(disabledPlugins.contains("*")){
+            return true;
+        }
+        if(pluginId == null || "".equals(pluginId)){
+            return true;
+        }
+        return disabledPlugins.contains(pluginId);
+    }
+
+    @Override
+    public void disablePlugin(String pluginId) {
+        if (isPluginDisabled(pluginId)) {
+            return;
+        }
+        disabledPlugins.add(pluginId);
+    }
+
+    @Override
+    public void enablePlugin(String pluginId) {
+        if (!isPluginDisabled(pluginId)) {
+            return;
+        }
+        disabledPlugins.remove(pluginId);
+    }
+}
